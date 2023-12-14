59dc07f145634faf04070878432a7f701ee0dda3
Batuhan Dilek <80598580+batuhan-dilek99@users.noreply.github.com>
2023-12-14 16:38:15 +0200

    code beautify

--- protocols/SDHHE.cpp
+++ protocols/SDHHE.cpp
@@ -1,3 +1,4 @@
+#define OPENSSL_API_COMPAT 0x10100000L
 #include <vector>
 #include <iostream>
 #include <string>
@@ -12,6 +13,7 @@
 #include "../src/pasta_3_seal.h"
 #include "../src/utils.h"
 #include "../src/sealhelper.h"
+#include "openssl/sha.h"
 using namespace std;
 using namespace seal;
 
@@ -29,6 +31,7 @@ class CSP{
         Ciphertext cipherUser;
         vector<Ciphertext> cprime;
         Ciphertext cres;
+        Ciphertext F;
     //Methods
 
         CSP(shared_ptr<SEALContext> context2){
@@ -54,13 +57,13 @@ class CSP{
             
         }
 
-        void processMessageFromAnalyst(SEALContext context, ifstream& file){
+        void processM1(SEALContext context, ifstream& file){
 
             chrono::high_resolution_clock::time_point start, end;
             chrono::milliseconds result;
 
             start = chrono::high_resolution_clock::now();
-            cout << "--- CSP is processing the first message ---" << endl;
+            cout << "--- CSP is processing M1 ---" << endl;
             //find symmetric key
             int symmetricKeyCommand = system("openssl rsautl --decrypt -inkey cspPriv.pem -in analystSymKey.txt.enc > analystSymKey.txt.dec 2> /dev/null");
             if(!symmetricKeyCommand){
@@ -97,8 +100,61 @@ class CSP{
             end = chrono::high_resolution_clock::now();
             cout << "[+] evk from analyst has been stored successfully" << endl;
             result = chrono::duration_cast<chrono::milliseconds>(end - start);
-            cout << "\t[?] Took " << result.count() << " milliseconds" << endl;
+            cout << "\t[?] Whole process took " << result.count() << " milliseconds" << endl;
             cout << "--- Done ---" << endl << endl;
+        }
+
+        void processM3(SEALContext context){
+
+            chrono::high_resolution_clock::time_point start, end;
+            chrono::milliseconds result;
+            start = chrono::high_resolution_clock::now();
+
+
+        //Validating Hash
+            int val = system("openssl dgst -verify analystPub.pem -keyform PEM -sha256 -signature hash.txt.sign -binary hash.txt");
+            if(!val){
+                cout << "[+] Signature is verified" << endl;
+            }
+        //----------------
+
+        //Extracting symkey from decrypted file
+            ifstream symkeyfile;
+            symkeyfile.open("analystSymKey.txt.dec");
+            string password;
+            string myText;
+            while (getline (symkeyfile, myText)) {
+            // Output the text from the file
+                password = myText;
+            }
+            symkeyfile.close();
+        //-------------------------------------
+
+        //Decrypt F function
+            string command1 = "openssl enc -d -aes-256-cbc -in fFunction.txt.enc -out fFunction.txt.dec --pass pass:";
+            string command2 = " 2> /dev/null";
+            string commandFull = string(command1) + string(password);// + string(command2);
+            int evkDecCommand = system(commandFull.c_str());
+            if(!evkDecCommand){
+                cout << "[+] evk file has been decrypted successfully" << endl;
+            }
+        //-----------------
+
+        //Read evk data into variable
+            ofstream file;
+            file.open("fFunction.txt.dec");
+            stringstream buffer;
+            buffer << file.rdbuf();
+            //this->F.unsafe_load(context, buffer);
+            //-----------------
+            end = chrono::high_resolution_clock::now();
+            cout << "[+] F function has been stored successfully" << endl;
+            result = chrono::duration_cast<chrono::milliseconds>(end - start);
+            cout << "\t[?] Took " << result.count() << " milliseconds" << endl;
+            file.close();
+            cout << "--- Done ---" << endl << endl;
+        //----------------------------
+
         }
         
         void processMessageFromUser(SEALContext context, ifstream& file, string userpass){
@@ -156,7 +212,7 @@ class CSP{
 class Analyst{
     
     public: 
-        //Attributes
+    //Attributes
         PublicKey hePub;
         SecretKey hePriv;
         RelinKeys heRelin;
@@ -167,7 +223,7 @@ class Analyst{
         GaloisKeys gk;
         vector<int64_t> f{17, 31, 24, 17, 32, 19};
         Ciphertext encF;
-        //Methods
+    //Methods
 
         Analyst(SEALContext context){
             cout << endl << "----------Constructor of Analyst------------" << endl;
@@ -225,12 +281,13 @@ class Analyst{
         }
         
         //Creating Message1
-        void createFirstMessage(SEALContext context, ofstream& file){
-            cout << endl <<"---Creating first message of Analyst---" << endl;
+        void createM1(SEALContext context, ofstream& file, ifstream& infile){
+            cout << endl <<"---Creating M1---" << endl;
             chrono::high_resolution_clock::time_point creatingM1Start, creatingM1End;
             chrono::milliseconds delta;
             
             creatingM1Start = chrono::high_resolution_clock::now();
+
             //Extraction of the relinerization key
             auto relSize = this->heRelin.save(this->relStream);
 
@@ -265,8 +322,69 @@ class Analyst{
             }
             creatingM1End = chrono::high_resolution_clock::now();
             delta = chrono::duration_cast<chrono::milliseconds>(creatingM1End - creatingM1Start);
-            cout << "[?] Creating the message took " << delta.count() << " milliseconds" << endl; 
+            cout << "\t[?] Forging M1 took " << delta.count() << " milliseconds" << endl; 
             cout << "---Done---" << endl << endl;
+        }
+
+        void createM3(shared_ptr<SEALContext> cc, SEALContext context, ofstream& file, ifstream& infile){
+
+            this->encryptF(cc);
+
+        //Generating Encryption file with CSP public key------------
+            this->encF.save(file); //Storing encrypted F result into file
+
+            //int encWithCSPpubkey = system("openssl rsautl -encrypt -inkey cspPub.pem -pubin -in fFunction.txt -out fFunction.txt.enc");  //Dosya adını yaz 
+            //Above line supposed to encrypt the f value with CSP public key. But it throws an error, saying the file is too big to encrypt. I am going to encrypt this one 
+            //like I have encrypted evk key. 
+
+            //Getting key from a text file
+            ifstream passwordFile("analystSymKey.txt");
+            string password;
+            string myText;
+            while (getline (passwordFile, myText)) {
+            // Output the text from the file
+                password = myText;
+            }
+            passwordFile.close();
+            //Constructing the command string for the signing operation
+            string signCommandString = string("openssl enc -aes-256-cbc -in analystRelFile.txt -out analystRelFile.txt.enc --pass pass:") + string(password) + string(" 2> /dev/null");
+            //signing evk with analyst signature
+            int signCommand = system(signCommandString.c_str());
+            if(!signCommand){
+                cout << "[+] F function is encrypted with CSP public key" << endl;
+            }
+        //------------------------------------------------------
+            
+            
+        //Generating the hash value-----------------------------
+            //initializing timestamp variable
+            size_t timestamp;
+            const auto now = std::chrono::system_clock::now();
+            timestamp = chrono::duration_cast<chrono::milliseconds>(now.time_since_epoch()).count();  //Getting system time
+            string timestampString = to_string(timestamp); //Data cast to string in order to be able to feed into buffer.
+
+            //Reading evk key from file
+            stringstream buffer;
+            buffer << infile.rdbuf();
+            string bufferToString = buffer.str();
+            
+            //Concat timestamp and evk
+            string finalValue = bufferToString + timestampString;
+            file << finalValue;
+
+            //Hashing
+            string echo = "echo '' > hash.txt";
+            string comm = "echo -n hash.txt | sha256sum > hash.txt"; //Hash value is stored in a file.
+            int command = system(comm.c_str());
+
+            //Signing the file
+            string sign = "openssl dgst -sign analystPriv.pem -keyform PEM -sha256 -out hash.txt.sign -binary hash.txt";
+            int signComm = system(sign.c_str());
+            if(!signComm){
+                cout << "[+] Hash value has been signed" << endl;
+            }
+
+        //------------------------------------------------------
         }
 
         void encryptF(shared_ptr<SEALContext> context){
@@ -281,7 +399,7 @@ class Analyst{
             end = chrono::high_resolution_clock::now();
             result = chrono::duration_cast<chrono::milliseconds>(end - start);
             cout << "[+] Analyst encrypted his input" << endl;
-            cout << "[?] Encrypting took " << result.count() << " milliseconds" << endl;
+            cout << "\t[?] Encrypting took " << result.count() << " milliseconds" << endl;
         }
 
         void decryptCres(shared_ptr<SEALContext> context, Ciphertext cres){
@@ -391,9 +509,23 @@ class User{
         }
 
         void sendMessageToCSP(shared_ptr<SEALContext> context, PublicKey analystHePub, ofstream& file, SecretKey he_sk, PublicKey he_pk, RelinKeys he_rk, GaloisKeys he_gk){
+            chrono::high_resolution_clock::time_point sskStart, sskEnd, writeStart, writeEnd, encStart, encEnd;
+            chrono::milliseconds ssk, write, enc;
+
+            sskStart = chrono::high_resolution_clock::now();
             this->encryptSSK(context, analystHePub, he_sk, he_pk, he_rk, he_gk);
+            sskEnd = chrono::high_resolution_clock::now();
+            cout << "\t[?] Took " << chrono::duration_cast<chrono::milliseconds>(sskEnd - sskStart).count() << " milliseconds" << endl;
+
+            writeStart = chrono::high_resolution_clock::now();
             this->writeCKandCintoFile(file);
+            writeEnd = chrono::high_resolution_clock::now();
+            cout << "\t[?] Took " << chrono::duration_cast<chrono::milliseconds>(writeEnd - writeStart).count() << " milliseconds" << endl;
+
+            encStart = chrono::high_resolution_clock::now();
             this->encryptCKCIFile();
+            encEnd = chrono::high_resolution_clock::now();
+            cout << "\t[?] Took " << chrono::duration_cast<chrono::milliseconds>(encEnd - encStart).count() << " milliseconds" << endl;
         }
 
 };
@@ -401,7 +533,7 @@ class User{
 
 int main(void){
 
-    //Setting parameters
+//Setting parameters
     //The reason that I am using two different context is, one context is for to be able to extract evk and ci's. Context2 is for evaluations and encryptions. 
     EncryptionParameters parms(scheme_type::bfv);
     parms.set_poly_modulus_degree(config::mod_degree);
@@ -409,78 +541,113 @@ int main(void){
     parms.set_plain_modulus(PlainModulus::Batching(config::mod_degree, 20));
     parms.set_plain_modulus(config::plain_mod);
     shared_ptr<SEALContext> context2 = get_seal_context(config::plain_mod, config::mod_degree, config::seclevel);
-    
-    //Setting SEAL context
+//-----------------------------    
+
+//Setting SEAL context
     SEALContext context(parms);
     print_parameters(context);
+//-----------------------------  
 
     chrono::high_resolution_clock::time_point setupStart, setupEnd, addStart, addEnd, queryStart, queryEnd;
     chrono::milliseconds deltaSetup, deltaAdd, deltaQuery;
 
     cout << endl << endl << "-*-*-*-*-*-*SD.SETUP START-*-*-*-*-*-*" << endl;
+
+//Construtcing classes--------
     CSP csp(context2);
     Analyst analyst(context);
 
     User us1;
+//----------------------------
+
     setupStart = chrono::high_resolution_clock::now(); 
-    //Opening out stream file for the communication between CSP and User
+//Opening out stream file for the communication between CSP and User
     ofstream outFileUser;
     outFileUser.open("userFile.txt");
-    //Opening in stream file for the communication between CSP and User
+//-----------------------------
+
+//Opening in stream file for the communication between CSP and User
     ifstream inFileUser;
     inFileUser.open("userFile.txt.dec");
+//-----------------------------
 
-
+//Constructing message 1------
     //Opening out stream file for the communication between CSP and Analyst
     ofstream outFile;
     outFile.open("analystRelFile.txt");
-    analyst.createFirstMessage(context, outFile);
+    ifstream relInFile;
+    relInFile.open("analystRelFile.txt");
+    ofstream hashOutFile;
+    hashOutFile.open("hash.txt");
+    ifstream hashInFile;
+    hashInFile.open("hash.txt");
+    analyst.createM1(context, outFile, relInFile);
+//-----------------------------
 
 
-    //Opening in stream file for the communication between CSP and Analyst
-    //analystRelFile.txt.enc
+//Opening in stream file for the communication between CSP and Analyst
     ifstream inFile;
     inFile.open("analystRelFile.txt.dec");
 
-    csp.processMessageFromAnalyst(context, inFile);
+    csp.processM1(context, inFile);
+//-----------------------------
     setupEnd = chrono::high_resolution_clock::now();
     cout << "-*-*-*-*-*-*SD.SETUP FINISHED-*-*-*-*-*-*" << endl;
     deltaSetup = chrono::duration_cast<chrono::milliseconds>(setupEnd - setupStart);
-    cout << "Total amount of time : " << deltaSetup.count() << " milliseconds"  << endl;
+    cout << "SD.Setup total amount of time : " << deltaSetup.count() << " milliseconds"  << endl;
 
 
 
     cout << endl << endl << "-*-*-*-*-*-*SD.ADD START-*-*-*-*-*-*" << endl;
     addStart = chrono::high_resolution_clock::now();
-    cout << endl << "User----" << endl;
+
+//User send message to CSP-----
+    cout << endl << "User Generating M2----" << endl;
     us1.sendMessageToCSP(context2, analyst.hePub, outFileUser, analyst.hePriv, analyst.hePub, analyst.heRelin, analyst.gk);
-    cout << "-----" << endl;
+    cout << "--- Done ---" << endl;  //hesapla
+//-----------------------------
+    
+//CSP operations---------------
     cout << endl << "CSP----" << endl;
     csp.processMessageFromUser(context, inFileUser, us1.pass);
     csp.decompression(context2, analyst.hePub, analyst.gk, us1.ck, us1.ci, analyst.sharedHeRelin);
     cout << "-----" << endl;
+//-----------------------------
+
+
     addEnd = chrono::high_resolution_clock::now();
     cout << "-*-*-*-*-*-*SD.ADD FINISHED-*-*-*-*-*-*" << endl;
     deltaAdd = chrono::duration_cast<chrono::milliseconds>(addEnd - addStart);
-    cout << "Total amount of time : " <<  deltaAdd.count() << " milliseconds" << endl;
+    cout << "SD.Add total amount of time : " <<  deltaAdd.count() << " milliseconds" << endl;
     
     cout << endl << endl << "-*-*-*-*-*-*SD.Query START-*-*-*-*-*-*" << endl;
     queryStart = chrono::high_resolution_clock::now();
-    analyst.encryptF(context2);
+
+//Constructing Message 3----
+    ofstream fFunction;
+    fFunction.open("fFunction.txt");
+    ifstream fFunctionIn;
+    fFunctionIn.open("fFunction.txt");
+    cout << "M3 being generated---" << endl;
+    analyst.createM3(context2, context, fFunction, fFunctionIn);
+    cout << "--- Done ---" << endl;
+//---------------------------
+
+//CSP operations-------------
+    cout << "M3 being processed---" << endl;
+    csp.processM3(context);
     csp.evaluate(context2, analyst.encF, analyst.hePub);
+//---------------------------
+
+//Analyst decrypt Res--------
     analyst.decryptCres(context2, csp.cres);
     queryEnd = chrono::high_resolution_clock::now();
+//---------------------------
     cout << "-*-*-*-*-*-*SD.Query FINISHED-*-*-*-*-*-*" << endl;
     deltaQuery = chrono::duration_cast<chrono::milliseconds>(queryEnd - queryStart);
-    cout << "Total amount of time : " <<  deltaQuery.count() << " milliseconds"  << endl;
+    cout << "SD.Query total amount of time : " <<  deltaQuery.count() << " milliseconds"  << endl;
 
-    // int fin = system("rm -rf analystPriv.pem | rm -rf analystPub.pem");
-
-    // User us1;
-    // us1.encryptSSK(context, analyst.hePub);
-    // us1.writeCKandCintoFile();
-    // if(!fin){
-    //     cout << "fin" << endl;
-    // }
+    
+    //int echo = system("echo '' | tee *.txt; echo '' | tee *.enc; echo '' | tee *.dec; echo '' | tee *.txt.dec; echo '' | tee *.txt.enc; echo '' | tee *.sign");
     return 0;
 }
